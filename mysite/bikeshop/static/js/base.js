bc83e5a2f67d1f7974181b7ceaea25ad1079daa1
Dsdubov <dubiman2011@gmail.com>
2016-02-16 21:46:55 +0300

    Design

---
+++ mysite/bikeshop/static/js/base.js
@@ -0,0 +1,31 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+/*********************
+Product box
+*********************/
+$(document).ready(function() {
+    $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
+    $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
+});
+/*********************
+Product box
+*********************/
--- mysite/bikeshop/urls.py
+++ mysite/bikeshop/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+import django
 from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='base'),
+	url(r'^media/(?P<path>.*)$', django.views.static.serve, name='photo'),
     url(r'^catalog/([a-z A-Z]+)/categories/(?P<category_name>[a-z A-Z]+)/products/$', views.product_list, name='product_list'),
     url(r'^catalog/$', views.catalog_list, name='catalog_list'),
     url(r'^catalog/(?P<catalog_name>[a-z A-Z]+)/categories/$', views.categories_list, name='categories_list'),
