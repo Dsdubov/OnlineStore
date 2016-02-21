fddcb58407f20279d6af923837d2b62ae6716c22
Dsdubov <dubiman2011@gmail.com>
2016-02-21 22:49:01 +0300

    Login edited

--- mysite/bikeshop/models.py
+++ mysite/bikeshop/models.py
@@ -64,38 +64,6 @@ class ProductAttribute(models.Model):
     def __str__(self):
         return self.name
 
-class LoginForm(forms.Form):
-    username = forms.CharField(label=u'Имя пользователя')
-    password = forms.CharField(label=u'Пароль', widget=forms.PasswordInput)
-
-    def clean(self):
-        cleaned_data = super(LoginForm, self).clean()
-        if not self.errors:
-            user = authenticate(username=cleaned_data['username'], password=cleaned_data['password'])
-            if user is None:
-                raise forms.ValidationError(u'Имя пользователя и пароль не подходят')
-            self.user = user
-        return cleaned_data
-
-    def get_user(self):
-        return self.user or None
-
-class RegistrationForm(forms.Form):
-    username = forms.CharField(label='Имя пользователя')
-    password1 = forms.CharField(label=u'Пароль', widget=forms.PasswordInput)
-    password2 = forms.CharField(label=u'Повторите пароль', widget=forms.PasswordInput)
-
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        # TODO: проверить, что username не занят
-        return self.cleaned_data
-
-    def clean(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        # TODO: проверить, что пароли совпадают
-        return self.cleaned_data
-
 class UserProfile(models.Model):
     # This line is required. Links UserProfile to a User model instance.
     user = models.OneToOneField(User)
--- mysite/bikeshop/views.py
+++ mysite/bikeshop/views.py
@@ -9,8 +9,8 @@ from django.shortcuts import render_to_response
 def start(request):
     return render(request, 'bikeshop/start.html')
 
-# def register(request):
-#     return render(request, 'bikeshop/register.html')
+def register(request):
+    return render(request, 'bikeshop/register.html')
     
 def chart(request):
     return render(request, 'bikeshop/chart.html')
@@ -58,10 +58,8 @@ def register(request):
         # Attempt to grab information from the raw form information.
         # Note that we make use of both UserForm and UserProfileForm.
         user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
-
         # If the two forms are valid...
-        if user_form.is_valid() and profile_form.is_valid():
+        if user_form.is_valid():
             # Save the user's form data to the database.
             user = user_form.save()
 
@@ -69,21 +67,6 @@ def register(request):
             # Once hashed, we can update the user object.
             user.set_password(user.password)
             user.save()
-
-            # Now sort out the UserProfile instance.
-            # Since we need to set the user attribute ourselves, we set commit=False.
-            # This delays saving the model until we're ready to avoid integrity problems.
-            profile = profile_form.save(commit=False)
-            profile.user = user
-
-            # Did the user provide a profile picture?
-            # If so, we need to get it from the input form and put it in the UserProfile model.
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-
-            # Now we save the UserProfile model instance.
-            profile.save()
-
             # Update our variable to tell the template registration was successful.
             registered = True
 
@@ -91,19 +74,19 @@ def register(request):
         # Print problems to the terminal.
         # They'll also be shown to the user.
         else:
-            print(user_form.errors, profile_form.errors)
+            print(user_form.errors)
 
     # Not a HTTP POST, so we render our form using two ModelForm instances.
     # These forms will be blank, ready for user input.
     else:
         user_form = UserForm()
-        profile_form = UserProfileForm()
 
     # Render the template depending on the context.
     return render_to_response(
-            'bikeshop/reg.html',
-            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered},
+            'bikeshop/register.html',
+            {'user_form': user_form, 'registered': registered},
             context)
+
 def user_login(request):
     # Like before, obtain the context for the user's request.
     context = RequestContext(request)
