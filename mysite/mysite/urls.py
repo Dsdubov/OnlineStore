c4fa0d6876ae33c04ab72976c81b00b221f0ccc2
Dsdubov <dubiman2011@gmail.com>
2016-02-23 18:17:04 +0300

    Orders added

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -7,6 +7,6 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^cart/', include('cart.urls', namespace="cart")),
-    url(r'^order/', include('orders.urls')),    
+    url(r'^orders/', include('orders.urls', namespace='orders')),    
     url(r'^', include('bikeshop.urls')),
 ] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
