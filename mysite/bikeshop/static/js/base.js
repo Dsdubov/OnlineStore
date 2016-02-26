be9ce4dabbd03e46c026f940ba00383b1f809f80
Dsdubov <dubiman2011@gmail.com>
2016-02-27 02:47:50 +0300

    Changing of navbar from static to fixed

--- mysite/bikeshop/static/js/base.js
+++ mysite/bikeshop/static/js/base.js
@@ -34,3 +34,16 @@ Product box
 function goBack() {
     window.history.back();
 }
+
+/****************************
+Static to fixed navbar
+****************************/
+$(document).scroll(function(e){
+    var scrollTop = $(document).scrollTop();
+    if(scrollTop > 0){
+        console.log(scrollTop);
+        $('.navbar').removeClass('navbar-static-top').addClass('navbar-fixed-top');
+    } else {
+        $('.navbar').removeClass('navbar-fixed-top').addClass('navbar-static-top');
+    }
+});
