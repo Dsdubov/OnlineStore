aab619198a16c8f1aa99b511b9adc76bf2744866
Dsdubov <dubiman2011@gmail.com>
2016-02-27 23:56:00 +0300

    Email sending added

--- mysite/orders/views.py
+++ mysite/orders/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
-from .tasks import order_created
+# from .tasks import order_created
 from cart.cart import Cart
+from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from .models import Order
 
+def order_created(order_id):
+    """
+    Task to send an e-mail notification when an order is successfully created.
+    """
+    order = Order.objects.get(id=order_id)
+    subject = 'Order nr. {}'.format(order.id)
+    mail = order.email
+    message = 'Dear {},\n\nYou have successfully placed an order. Your order id is {}.'.format(order.first_name, order.id)
+    mail_sent = EmailMessage(subject, message, to=[mail,])
+    # mail_sent = send_mail(subject, message, 'admin@bikeshop.com', ['dubiman2011@gmail.com',])
+    return mail_sent.send()
 
 def order_create(request):
     cart = Cart(request)
@@ -21,7 +35,7 @@ def order_create(request):
             # clear the cart
             cart.clear()
             # launch asynchronous task
-            # order_created.delay(order.id)
+            order_created(order.id)
             return render(request, 'orders/order/created.html', {'order': order})
         else:
             print("Not valid")
