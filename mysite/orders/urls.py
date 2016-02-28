dd7d747b5981e141d578229be103bcfaa3a22749
Dsdubov <dubiman2011@gmail.com>
2016-02-28 12:02:08 +0300

    Order status edited

--- mysite/orders/urls.py
+++ mysite/orders/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^create/$', views.order_create, name='order_create'),
-    url(r'^status/$', views.status, name='order_create'),
+    url(r'^status/$', views.status, name='check_status'),
 ]
