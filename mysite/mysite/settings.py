be6d8316bc9c0fd33cc2779e0b83b54de5583d7d
Dsdubov <dubiman2011@gmail.com>
2016-06-23 10:22:18 +0300

    MySQL connected

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,10 +83,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'buybike',
+        'USER': 'dsdubov',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
